329bc99c6f35500a80d54d1e4f96a2aed721d3fd
Przemyslaw Pokrywka <przemyslaw.pokrywka@gmail.com>
2011-12-17 22:26:25 +0100

    Pasted licence formula everywhere, adjusted README with respect to optional params

--- src/main/java/com/blstream/smppserver/PacketProcessor.java
+++ src/main/java/com/blstream/smppserver/PacketProcessor.java
@@ -4,6 +4,23 @@ import ie.omk.smpp.message.SMPPPacket;
 import ie.omk.smpp.message.SMPPResponse;
 
 /**
+ * Copyright 2011 German Escobar
+ * Copyright 2011 BLStream
+ * Copyright 2011 Przemysław Pokrywka <przemyslaw.pokrywka@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
  * This interface is implemented by those who want to process incoming SMPP packets received in a 
  * {@link SmppSession}.
  * 
--- src/main/java/com/blstream/smppserver/util/PacketFactory.java
+++ src/main/java/com/blstream/smppserver/util/PacketFactory.java
@@ -44,15 +44,32 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PacketFactory {
-
 /**
+ * Copyright 2011 German Escobar
+ * Copyright 2011 BLStream
+ * Copyright 2011 Przemysław Pokrywka <przemyslaw.pokrywka@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * 
  * @author German Escobar
  * 
  * THIS FILE HAS BEEN CHANGED IN RELATION TO THE ORIGINAL VERSION BY
  * @author Przemyslaw Pokrywka
- * 
+ *
  */
+public class PacketFactory {
+
 private static final Logger LOG = LoggerFactory.getLogger(PacketFactory.class);
     
     private final Map<Integer, Class<? extends SMPPPacket>> commands =
